72cf98034be2d809b33423549251e2a7b7f10c7d
Stef Walter <stefw@redhat.com>
2015-06-09 23:24:05 +0200

    FEDORA demo changes

--- record.sh
+++ record.sh
@@ -3,7 +3,7 @@
 set -x
 
 recordmydesktop \
-	-x 1680 --width 1920 --height 1080 \
+	-x 1700 -y 40 --width 1600 --height 900 \
 	--fps 24 --freq 44100 \
 	--workdir /opt/tmp \
 	-o /opt/tmp/demo.ogv
