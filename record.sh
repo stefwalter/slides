4603726a8870fea97704239105b2febf48b356cf
Stef Walter <stefw@redhat.com>
2015-06-11 15:20:02 +0200

    Fedora Demo stuff

--- record.sh
+++ record.sh
@@ -3,7 +3,9 @@
 set -x
 
 recordmydesktop \
-	-x 1700 -y 40 --width 1600 --height 900 \
+	-x 1700 -y 150 --width 1600 --height 900 \
 	--fps 24 --freq 44100 \
 	--workdir /opt/tmp \
 	-o /opt/tmp/demo.ogv
+
+# ffmpeg -i cockpit-atomic-kube-2015-2.ogv -vn -c copy cockpit-atomic-kube-2015-2.ogg
