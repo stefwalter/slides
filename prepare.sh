b38b73e46f38139b8d3c7a745a6866e384c64717
Stef Walter <stefw@redhat.com>
2016-01-22 15:53:20 +0100

    Add some videos

--- prepare.sh
+++ prepare.sh
@@ -1,7 +1,10 @@
 #!/bin/sh
 
+sudo hostnamectl set-hostname 'falcon.thewalter.lan'
+sudo hostnamectl set-hostname --pretty 'Falcon'
 sudo brctl addbr eth5
 sudo brctl addbr eth6
 sudo virsh net-destroy cockpit
 sudo virsh net-destroy cockpit1
+sudo rm /var/lib/cockpit/*
 
