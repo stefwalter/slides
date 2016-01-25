432963489376f3722600aaf52e0715b7fce8bdfc
Stef Walter <stefw@redhat.com>
2016-01-25 17:01:22 +0100

    Update gitigtore

--- prepare.sh
+++ prepare.sh
@@ -4,7 +4,7 @@ sudo hostnamectl set-hostname 'falcon.thewalter.lan'
 sudo hostnamectl set-hostname --pretty 'Falcon'
 sudo brctl addbr eth5
 sudo brctl addbr eth6
-sudo virsh net-destroy cockpit
-sudo virsh net-destroy cockpit1
+# sudo virsh net-destroy cockpit
+# sudo virsh net-destroy cockpit1
 sudo rm /var/lib/cockpit/*
 
