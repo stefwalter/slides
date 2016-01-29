d67c3162cca72c4de09437bfc68514b820d25f50
Stef Walter <stefw@redhat.com>
2016-01-29 09:35:06 +0100

    recent changes

--- record.sh
+++ record.sh
@@ -12,10 +12,10 @@ H=480
 W=1024
 H=768
 
-# recordmydesktop -x $X -y $Y --width $W --height $H --v_bitrate 2000000 \
-#	--fps 24 --freq 44100  --workdir /opt/tmp -o ./demo.ogv
+recordmydesktop -x $X -y $Y --width $W --height $H --v_bitrate 2000000 \
+	--fps 24 --freq 44100  --workdir /opt/tmp -o ./demo.ogv
 
 # Build with https://github.com/stefwalter/byzanz
 # * No loops
 # * Handle Ctrl-C right
-byzanz-record --cursor --x=$X --y=$Y --width=$W --height=$H --duration=1500000 ./demo.webm
+# byzanz-record --audio --cursor --x=$X --y=$Y --width=$W --height=$H --duration=1500000 ./demo.webm
