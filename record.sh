7c2a5b125a1114c557c59278bce66b68a2f4e0c9
Stef Walter <stefw@redhat.com>
2016-06-17 16:00:04 +0200

    Add updated videos

--- record.sh
+++ record.sh
@@ -4,10 +4,11 @@ set -x
 
 recordmydesktop \
 	-x 1680 -y 200 --width 1024 --height 576 \
-	--fps 24 --freq 44100 --delay 3 \
+	--fps 24 --freq 44100 \
 	--workdir /opt/tmp \
 	--v_bitrate 2000000 \
 	-o /opt/tmp/demo.ogv
 
+# --delay 3 \
 	# -x 1690 -y 30 --width 1280 --height 720 \
 # ffmpeg -i cockpit-atomic-kube-2015-2.ogv -vn -c copy cockpit-atomic-kube-2015-2.ogg
