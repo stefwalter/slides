7ebd91be2c48db47713d3b6d257451dedd86c612
Stef Walter <stefw@redhat.com>
2016-06-21 13:55:17 +0200

    Rebased onto reveal.js

--- summit/slides.js
+++ summit/slides.js
@@ -14,20 +14,20 @@ Reveal.initialize({
 
 	// Optional reveal.js plugins
 	dependencies: [
-		{ src: 'reveal/lib/js/classList.js', condition: function() {
+		{ src: '../reveal/lib/js/classList.js', condition: function() {
 			return !document.body.classList; }
 		},
-		{ src: 'reveal/plugin/markdown/marked.js', condition: function() {
+		{ src: '../reveal/plugin/markdown/marked.js', condition: function() {
 			return !!document.querySelector( '[data-markdown]' ); }
 		},
-		{ src: 'reveal/plugin/markdown/markdown.js', condition: function() {
+		{ src: '../reveal/plugin/markdown/markdown.js', condition: function() {
 			return !!document.querySelector( '[data-markdown]' ); }
 		},
-		{ src: 'reveal/plugin/highlight/highlight.js', async: true, callback: function() {
+		{ src: '../reveal/plugin/highlight/highlight.js', async: true, callback: function() {
 			hljs.initHighlightingOnLoad(); }
 		},
-		{ src: 'reveal/plugin/zoom-js/zoom.js', async: true },
-		{ src: 'reveal/plugin/notes/notes.js', async: true }
+		{ src: '../reveal/plugin/zoom-js/zoom.js', async: true },
+		{ src: '../reveal/plugin/notes/notes.js', async: true }
 	]
 });
 
@@ -47,5 +47,5 @@ Reveal.addEventListener('ready', followFooter);
 var link = document.createElement( 'link' );
 link.rel = 'stylesheet';
 link.type = 'text/css';
-link.href = window.location.search.match( /print-pdf/gi ) ? 'reveal/css/print/pdf.css' : 'reveal/css/print/paper.css';
+link.href = window.location.search.match( /print-pdf/gi ) ? '../reveal/css/print/pdf.css' : '../reveal/css/print/paper.css';
 document.getElementsByTagName( 'head' )[0].appendChild( link );
