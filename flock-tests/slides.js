c1ea75755ac4b46da5909dab3d7f119903ec49b9
Stef Walter <stefw@redhat.com>
2017-08-23 13:56:30 +0200

    Flock talks

---
+++ flock-tests/slides.js
@@ -0,0 +1,16 @@
+/* Included after reveal.js */
+Reveal.initialize({
+    width: "100%",
+    height: "100%",
+    controls: true,
+    progress: true,
+    history: true,
+    center: true,
+
+    keyboard: {
+        40: 'right',
+        38: 'left'
+    },
+
+    transition: 'slide', // none/fade/slide/convex/concave/zoom
+});
