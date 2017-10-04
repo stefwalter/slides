47bf5d48dae41ff344cde8f6c4de2943f2745835
Stef Walter <stefw@redhat.com>
2017-10-04 14:08:25 +0200

    WIP

---
+++ cyborg-teams/slides.js
@@ -0,0 +1,27 @@
+/* Included after reveal.js */
+Reveal.initialize({
+    controls: true,
+    progress: true,
+    history: true,
+    center: true,
+
+    	// The "normal" size of the presentation, aspect ratio will be preserved
+	// when the presentation is scaled to fit different resolutions. Can be
+	// specified using percentage units.
+	width: 1060,
+	height: 800,
+
+	// Factor of the display size that should remain empty around the content
+	margin: 0.1,
+
+	// Bounds for smallest/largest possible scale to apply to content
+	minScale: 0.2,
+	maxScale: 1.5,
+
+    keyboard: {
+        40: 'right',
+        38: 'left'
+    },
+
+    transition: 'slide', // none/fade/slide/convex/concave/zoom
+});
